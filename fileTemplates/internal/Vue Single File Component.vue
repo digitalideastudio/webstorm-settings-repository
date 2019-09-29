b1bfcb7e4cce2a203711c6f308720ea18a588323
Serhii Matrunchyk <serhii@digitalidea.studio>
2019-09-29 13:50:36 +0300

    first commit

---
+++ fileTemplates/internal/Vue Single File Component.vue
@@ -0,0 +1,16 @@
+<template>
+#[[$END$]]#
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default class ${COMPONENT_NAME} extends Vue {
+}
+</script>
+
+<style
+  lang="less"
+  scoped
+>
+</style>
